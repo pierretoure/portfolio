ee58388e7d7284cfe6a0f5698d077aa789950f6f
Pierre <pierre.tr@hotmail.fr>
2020-11-23 17:09:33 +0100

    fix: open pdf in new tab

--- src/components/CV/CV.tsx
+++ src/components/CV/CV.tsx
@@ -6,7 +6,7 @@ import './CV.css';
 
 export default function CV() {
 	return (
-		<a href={CVpdf} rel="noopener noreferrer" target="_blank" className='profile-cv'>
+		<a href={CVpdf} target="_blank" className='profile-cv'>
 			<CVIcon size={46} />
 			<span style={{ marginLeft: 8 }}>télécharger le CV</span>
 		</a>
